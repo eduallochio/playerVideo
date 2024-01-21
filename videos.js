c95e57e5159dff93e53a0ba21ec7917e52c81cc9
eduallochio.github.io <43136553+eduallochio@users.noreply.github.com>
2024-01-21 17:20:55 -0300

    playerList

---
+++ videos.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    src: "./img/01.mp4",
+    title: "Pitolo parte 1",
+  },
+  {
+    src: "./img/02.mp4",
+    title: "Pitolo parte 2",
+  },
+  {
+    src: "./img/03.mp4",
+    title: "Pitolo parte 3",
+  },
+  {
+    src: "./img/04.mp4",
+    title: "Pitolo parte 4",
+  },
+  {
+    src: "./img/05.mp4",
+    title: "Pitolo parte 5",
+  },
+  {
+    src: "./img/06.mp4",
+    title: "Pitolo parte 6",
+  },
+  
+]
